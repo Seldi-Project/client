74c657f5d3655fd05b3683c75e79746211cb50ae
shinsojeong <shinsojeong82@gmail.com>
2021-11-17 10:06:49 +0900

    start 버튼 수정

--- src/component/start/Start.js
+++ src/component/start/Start.js
@@ -1,10 +1,12 @@
-// @flow
 import * as React from 'react';
+import { useNavigate } from 'react-router';
 import '../../style/start.scss';
 import { ReactComponent as Logo } from '../../source/Logo.svg';
 import { ReactComponent as ExpandRight } from '../../source/ExpandRight.svg';
 
 export default function Start() {
+  const navigate = useNavigate();
+
   return (
     <div className="component" id="startComponent">
 
@@ -18,8 +20,13 @@ export default function Start() {
         <p id="startContent2">안전한 학교생활, 셀디로 시작해보세요.</p>
       </div>
 
-      <div className="btn" id="btnWhite">
-        <span className="btnMsg">학교 선택 후 시작하기</span>
+      <div className="btn" id="btnWhite" onClick={() => navigate("/login")}>
+        <span className="btnMsg">로그인</span>
+        <ExpandRight/>
+      </div>
+
+      <div className="btn" id="btnWhite" onClick={() => navigate("/join")}>
+        <span className="btnMsg">회원가입</span>
         <ExpandRight/>
       </div>
 
