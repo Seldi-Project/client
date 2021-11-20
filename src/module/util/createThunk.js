63e302004ebf950bdfdb9f839009c74e81ca4495
shinsojeong <shinsojeong82@gmail.com>
2021-11-20 19:14:23 +0900

    createThunk 수정

--- src/module/util/createThunk.js
+++ src/module/util/createThunk.js
@@ -1,25 +1,26 @@
 export const createThunk = (
   type,
   func,
-  pushOption,
-  path
+  messageState
 ) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
   return ((param) => async(dispatch) => {
     dispatch({ type, param });  //시작
-    try {
-      const payload = await func(param);
-      const payloadData = payload.payloadData || payload.res.data.data;
 
-      if (payload.res.data.code === 200) {
-        dispatch({ type: SUCCESS, payload: payloadData });  //성공
-        if (pushOption === true) {
-          window.location.replace(path);
-        }
-      } else if (payload.res.data.code === 403) {
-        window.location.replace("/");
+    try {
+      const { res: { data }, updateData } = await func(param);
+
+      if (updateData) {  //단순 업데이트였으면
+        await dispatch({ type: SUCCESS, payload: updateData })
+      } else {
+        await dispatch({ type: SUCCESS, payload: data });
       }
+      
+      if (messageState) {
+        alert(data.description);
+      }
+      return data;
     } catch (e) {
       console.log(e);
       dispatch({ type: ERROR, payload: e });  //실패
