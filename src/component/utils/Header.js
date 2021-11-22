c725a0bb09940a4865e1ad8349911837afd37449
shinsojeong <shinsojeong82@gmail.com>
2021-11-22 15:39:19 +0900

    Header 수정 (left, title 인수에 따르도록 수정) + scss 수정

--- src/component/utils/Header.js
+++ src/component/utils/Header.js
@@ -3,16 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import { ReactComponent as ExpandLeft } from '../../source/ExpandLeft.svg';
 import '../../style/header.scss';
 
-export default function TopBar({ title }) {
+export default function Header({ left, title, number }) {
   const navigate = useNavigate();
 
   return (
     <div className="header">
-      <div className="left" onClick={() => navigate(-1)}>
-        <ExpandLeft/>
-      </div>
-
-      <p className="center">{title}</p>
+      {left &&
+        <div className="left" onClick={() => navigate(-1)}>
+          <ExpandLeft/>
+        </div>  
+      }
+      {title==="join" ?
+        <div className="process">
+          <span className="processItem" id={number===1?"selected":"none"}></span>
+          <span className="processItem" id={number===2?"selected":"none"}></span>
+          <span className="processItem" id={number===3?"selected":"none"}></span>
+          <span className="processItem" id={number===4?"selected":"none"}></span>
+          <span className="processItem" id={number===5?"selected":"none"}></span>
+        </div>
+      :
+        <p className="center">{title}</p>
+      }
     </div>
   );
 }
